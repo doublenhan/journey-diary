b2b226b52d9a99c74c092b327a95ae7f0f3f35df
NhanVo <hoangnhanvo17@gmail.com>
2025-11-30 21:46:20 -0600

    Remove DebugEnv component - clean production build

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ import ViewMemory from './ViewMemory';
 import AnniversaryReminders from './AnniversaryReminders';
 import SettingPage from './SettingPage';
 import LoginPage from './LoginPage';
-import { DebugEnv } from './DebugEnv';
 import './styles/App.css';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
@@ -350,7 +349,6 @@ function App() {
       <Route path="/anniversary-reminders" element={<AnniversaryReminders onBack={() => window.history.back()} currentTheme={currentTheme} />} />
       <Route path="/setting-page" element={<SettingPage onBack={() => window.history.back()} currentTheme={currentTheme} setCurrentTheme={setCurrentTheme} />} />
     </Routes>
-    <DebugEnv />
     </>
   );
 }
